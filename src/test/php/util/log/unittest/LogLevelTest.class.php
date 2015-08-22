5d7150b0e8c194ae58be327f6b7c87ee102110b1
Timm Friebe <thekid@thekid.de>
2015-08-22 18:21:38 +0200

    Also test LogLevel::nameOf()

--- src/test/php/util/log/unittest/LogLevelTest.class.php
+++ src/test/php/util/log/unittest/LogLevelTest.class.php
@@ -6,32 +6,57 @@ use util\log\LogLevel;
 class LogLevelTest extends TestCase {
 
   #[@test]
-  public function namedInfo() {
+  public function named_info() {
     $this->assertEquals(LogLevel::INFO, LogLevel::named('INFO'));
   }
 
   #[@test]
-  public function namedWarn() {
+  public function named_warn() {
     $this->assertEquals(LogLevel::WARN, LogLevel::named('WARN'));
   }
 
   #[@test]
-  public function namedError() {
+  public function named_error() {
     $this->assertEquals(LogLevel::ERROR, LogLevel::named('ERROR'));
   }
 
   #[@test]
-  public function namedDebug() {
+  public function named_debug() {
     $this->assertEquals(LogLevel::DEBUG, LogLevel::named('DEBUG'));
   }
 
   #[@test]
-  public function namedAll() {
+  public function named_all() {
     $this->assertEquals(LogLevel::ALL, LogLevel::named('ALL'));
   }
 
   #[@test, @expect('lang.IllegalArgumentException')]
   public function unknown() {
     LogLevel::named('@UNKNOWN@');
+  }
+
+  #[@test]
+  public function nameOf_info() {
+    $this->assertEquals('INFO', LogLevel::nameOf(LogLevel::INFO));
+  }
+
+  #[@test]
+  public function nameOf_warn() {
+    $this->assertEquals('WARN', LogLevel::nameOf(LogLevel::WARN));
+  }
+
+  #[@test]
+  public function nameOf_error() {
+    $this->assertEquals('ERROR', LogLevel::nameOf(LogLevel::ERROR));
+  }
+
+  #[@test]
+  public function nameOf_debug() {
+    $this->assertEquals('DEBUG', LogLevel::nameOf(LogLevel::DEBUG));
+  }
+
+  #[@test, @expect('lang.IllegalArgumentException')]
+  public function nameOf_illegal_loglevel() {
+    LogLevel::nameOf(-1);
   }
 }
