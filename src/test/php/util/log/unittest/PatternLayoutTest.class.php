8662ad06c6915064d98ca3a411466547b140e6c5
Timm Friebe <thekid@thekid.de>
2015-08-22 14:44:45 +0200

    Add tests for all format tokens

--- src/test/php/util/log/unittest/PatternLayoutTest.class.php
+++ src/test/php/util/log/unittest/PatternLayoutTest.class.php
@@ -5,6 +5,7 @@ use util\log\context\MappedLogContext;
 use util\log\LoggingEvent;
 use util\log\LogLevel;
 use util\log\LogCategory;
+use lang\IllegalArgumentException;
 
 class PatternLayoutTest extends \unittest\TestCase {
 
@@ -24,42 +25,63 @@ class PatternLayoutTest extends \unittest\TestCase {
     );   
   }
 
-  #[@test, @expect('lang.IllegalArgumentException')]
-  public function illegalFormatToken() {
+  #[@test, @expect(IllegalArgumentException::class)]
+  public function illegal_format_token() {
     new PatternLayout('%Q');
   }
 
-  #[@test, @expect('lang.IllegalArgumentException')]
-  public function unterminatedFormatToken() {
+  #[@test, @expect(IllegalArgumentException::class)]
+  public function unterminated_format_token() {
     new PatternLayout('%');
   }
 
   #[@test]
-  public function literalPercent() {
-    $this->assertEquals(
-      '100%',
-      (new PatternLayout('100%%'))->format($this->newLoggingEvent())
-    );
+  public function message() {
+    $this->assertEquals('Hello', (new PatternLayout('%m'))->format($this->newLoggingEvent()));
   }
 
   #[@test]
-  public function simpleFormat() {
-    $this->assertEquals(
-      'WARN [default] Hello',
-      (new PatternLayout('%L [%c] %m'))->format($this->newLoggingEvent())
-    );
+  public function category_name() {
+    $this->assertEquals('default', (new PatternLayout('%c'))->format($this->newLoggingEvent()));
   }
 
   #[@test]
-  public function defaultFormat() {
-    $this->assertEquals(
-      '[16:08:04 1214 warn] Hello',
-      (new PatternLayout('[%t %p %l] %m'))->format($this->newLoggingEvent())
-    );
+  public function lowercase_loglevel() {
+    $this->assertEquals('warn', (new PatternLayout('%l'))->format($this->newLoggingEvent()));
   }
 
   #[@test]
-  public function tokenContext() {
+  public function uppercase_loglevel() {
+    $this->assertEquals('WARN', (new PatternLayout('%L'))->format($this->newLoggingEvent()));
+  }
+
+  #[@test]
+  public function time_in_HH_MM_SS() {
+    $this->assertEquals('16:08:04', (new PatternLayout('%t'))->format($this->newLoggingEvent()));
+  }
+
+  #[@test]
+  public function process_id() {
+    $this->assertEquals('1214', (new PatternLayout('%p'))->format($this->newLoggingEvent()));
+  }
+
+  #[@test]
+  public function literal_percent() {
+    $this->assertEquals('100%', (new PatternLayout('100%%'))->format($this->newLoggingEvent()));
+  }
+
+  #[@test]
+  public function line_break() {
+    $this->assertEquals("\n", (new PatternLayout('%n'))->format($this->newLoggingEvent()));
+  }
+
+  #[@test]
+  public function context_when_not_available() {
+    $this->assertEquals('', (new PatternLayout('%x'))->format($this->newLoggingEvent()));
+  }
+
+  #[@test]
+  public function context() {
     $context= new MappedLogContext();
     $context->put('key1', 'val1');
 
@@ -68,4 +90,21 @@ class PatternLayoutTest extends \unittest\TestCase {
       (new PatternLayout('%x'))->format($this->newLoggingEvent(new LogCategory('default', LogLevel::ALL, $context)))
     );
   }
+
+  #[@test]
+  public function simple_format() {
+    $this->assertEquals(
+      'WARN [default] Hello',
+      (new PatternLayout('%L [%c] %m'))->format($this->newLoggingEvent())
+    );
+  }
+
+  #[@test]
+  public function default_format() {
+    $this->assertEquals(
+      '[16:08:04 1214 warn] Hello',
+      (new PatternLayout('[%t %p %l] %m'))->format($this->newLoggingEvent())
+    );
+  }
+
 }
